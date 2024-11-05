3707e164c8a23ffe6172b7aa05fb686ffda29270
kauane-zanchi <kauane.zanchi@escola.pr.gov.br>
2024-11-05 16:53:48 -0300

    05/11

--- DadosGlobais.js
+++ DadosGlobais.js
@@ -1,22 +1,31 @@
 const url = `https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json`
 
-console.log(url);
 
 async function visualizarInformacoesGlobais(){
     const res = await fetch(url);
     const dados = await res.json();
+const PessoasMundo = ((dados.total_pessoas_mundo)/1e9).toFixed(2)
+const PessoasConectadas = ((dados.total_pessoas_conectadas)/1e9).toFixed(2)
+const Horas = parseInt(dados.tempo_medio)
+const Minutos = Math.round ((dados.tempo_medio - Horas)*100).toFixed(2)
+const Porcentagem = ((PessoasConectadas/PessoasMundo)*100).toFixed(2)
+
+
+
     const paragrafo = document.createElement(`p`);
     paragrafo.classList.add(`graficos-container__texto`);
     paragrafo.innerHTML = `Você sabia que o mundo tem 
-    <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente 
-    <span> ${dados.total_pessoas_conectadas} </span>estão conectadas em 
+    <span>${PessoasMundo}</span> bilhões de pessoas e que aproximadamente 
+    <span> ${PessoasConectadas} bilhões </span>estão conectadas em 
     alguma rede social e passam em média 
-    <span>${dados.tempo_medio}</span> horas conectadas`
+    <span>${Horas}</span> horas e <span> ${Minutos} </span> minutos conectadas
+    <br> Isto é impressionante pois cerca de <span> ${Porcentagem} % </span> de pessoas
+    no mundo utulizam alguma rede social.
+    `
 
 const container = document.getElementById(`graficos-container`);
 container.appendChild(paragrafo);
 
-    console.log(paragrafo);
 }
 
 visualizarInformacoesGlobais()
